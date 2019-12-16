e564622a0d52d0ad75b0f09315ef76943a2c2dd2
ignasvait <vaitkusignas@gmail.com>
2019-12-16 18:35:09 +0200

    Sukuriu pradmenis, tikrinancius ar ivestas zodis yra is skaiciu. PASTABA: Programa dar vis neveikia

--- NamuDarbas1/NamuDarbas1/Program.cs
+++ NamuDarbas1/NamuDarbas1/Program.cs
@@ -6,7 +6,34 @@ namespace NamuDarbas1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string skaicius = Console.ReadLine();
+            bool arTaiSkaicius = false;
+            for (int i = 0; i < skaicius.Length; i++)
+            {
+                
+                char simbolis = skaicius[i];
+                
+                if (simbolis <= 0 || simbolis > 0) arTaiSkaicius = true;
+                else
+                {
+                    arTaiSkaicius = false;
+                    break;
+                }
+
+            }
+
+            if (arTaiSkaicius==true)
+            {
+                Console.WriteLine("Valioo!");
+
+            }
+            else
+            {
+                Console.WriteLine("Ivestas neteisingas skaicius, bandykite is naujo");
+
+                
+            }
+
         }
     }
 }
