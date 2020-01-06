e78d3008426dd9b8145caf690de8cb02403848aa
ignasvait <vaitkusignas@gmail.com>
2020-01-06 23:46:09 +0200

    Programa pilnai veikia su skaiciais nuo -9 iki 9, paraso juos zodziu

--- NamuDarbas1/NamuDarbas1/Program.cs
+++ NamuDarbas1/NamuDarbas1/Program.cs
@@ -6,6 +6,7 @@ namespace NamuDarbas1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite skaiciu nuo -9 iki 9 ");
             string skaicius = Console.ReadLine();
             bool arTaiSkaicius = false;
             
@@ -23,27 +24,52 @@ namespace NamuDarbas1
 
             }
 
-            if (arTaiSkaicius==true)
-            {
-                Console.WriteLine("Valioo!");
-
-            }
-            else
+            if (arTaiSkaicius == false)
             {
                 Console.WriteLine("Ivestas neteisingas skaicius, bandykite is naujo");
                 return;
-                
-            }
+            }   
+            
             
             int skaicius1 = Convert.ToInt32(skaicius);
             if (TeisingiReziai(skaicius1)==false) return;
-            Console.WriteLine("Ivestas teisingas skaicius");
+
+            Console.WriteLine(VienzenkliaiSkaiciai(skaicius1));
+
         }
 
         static bool TeisingiReziai(int skaitmuo)
         {
             if (skaitmuo > -10 && skaitmuo < 10) return true;
             else return false;
+        }
+
+        static string VienzenkliaiSkaiciai(int skaitmuo)
+        {
+            string word="";
+            string zodis = "";
+
+
+            if (skaitmuo < 0)
+            { skaitmuo = skaitmuo * -1;
+                zodis = "minus "; }
+                
+                switch (skaitmuo)
+            {
+                    case 1:  zodis=zodis+"vienas";  break;
+                    case 2:  zodis=zodis+"du";    break;
+                    case 3:  zodis=zodis+"trys";    break;
+                    case 4:  zodis=zodis+"keturi";  break;
+                    case 5:  zodis=zodis+"penki";   break;
+                    case 6:  zodis=zodis+"sesi";    break;
+                    case 7:  zodis=zodis+"septyni"; break;
+                    case 8:  zodis=zodis+"astuoni"; break;
+                    case 9:  zodis=zodis+"devyni";  break;
+                    case 0:  zodis=zodis+"nulis";   break;
+
+            }
+
+            return zodis;
         }
     }
 }
