e6fba7cb57ecc075b73cae5cec8d2d7f1fbd9536
ignasvait <vaitkusignas@gmail.com>
2020-01-08 17:48:56 +0200

    Prideta dvizenkliu skaiciu funkcija, dar neveikia!

--- NamuDarbas1/NamuDarbas1/Program.cs
+++ NamuDarbas1/NamuDarbas1/Program.cs
@@ -34,13 +34,15 @@ namespace NamuDarbas1
             int skaicius1 = Convert.ToInt32(skaicius);
             if (TeisingiReziai(skaicius1)==false) return;
 
-            Console.WriteLine(VienzenkliaiSkaiciai(skaicius1));
+            if(skaicius1>-10 && skaicius1<10) Console.WriteLine(VienzenkliaiSkaiciai(skaicius1));
+            else if (skaicius1>-20 && skaicius1<20) Console.WriteLine(VienzenkliaiSkaiciai(skaicius1));
+            else Console.WriteLine(DvizenkliaiSkaiciai(skaicius1) + " " + VienzenkliaiSkaiciai(skaicius1));
 
         }
 
         static bool TeisingiReziai(int skaitmuo)
         {
-            if (skaitmuo > -10 && skaitmuo < 10) return true;
+            if (skaitmuo > -20 && skaitmuo < 20) return true;
             else return false;
         }
 
@@ -70,6 +72,56 @@ namespace NamuDarbas1
             }
 
             return zodis;
+        }
+
+        static string DvizenkliaiSkaiciai(int skaitmuo)
+        {
+            string word = "";
+            string zodis = "";
+
+
+            if (skaitmuo < 0)
+            {
+                skaitmuo = skaitmuo * -1;
+                zodis = "minus ";
+            }
+            if (skaitmuo > 9 && skaitmuo < 20)
+            {
+                switch (skaitmuo)
+                {
+                    case 10: zodis = zodis + "desimt"; break;
+                    case 11: zodis = zodis + "vienuolika"; break;
+                    case 12: zodis = zodis + "dvylika"; break;
+                    case 13: zodis = zodis + "trylika"; break;
+                    case 14: zodis = zodis + "keturiolika"; break;
+                    case 15: zodis = zodis + "penkiolika"; break;
+                    case 16: zodis = zodis + "sesiolika"; break;
+                    case 17: zodis = zodis + "septyniolika"; break;
+                    case 18: zodis = zodis + "astuoniolika"; break;
+                    case 19: zodis = zodis + "devyniolika"; break;
+                }
+            }
+            else
+            {
+                skaitmuo = skaitmuo / 10;
+
+                switch (skaitmuo)
+                {
+                    
+                    case 2: zodis = zodis + "dvidesimt"; break;
+                    case 3: zodis = zodis + "trisdesimt"; break;
+                    case 4: zodis = zodis + "keturiasdesimt"; break;
+                    case 5: zodis = zodis + "penkiasdesimt"; break;
+                    case 6: zodis = zodis + "sesiasdesimt"; break;
+                    case 7: zodis = zodis + "septyniasdesimt"; break;
+                    case 8: zodis = zodis + "astuoni"; break;
+                    case 9: zodis = zodis + "devyniasdesimt"; break;
+                    
+
+                }
+            }
+            return zodis;
+
         }
     }
 }
